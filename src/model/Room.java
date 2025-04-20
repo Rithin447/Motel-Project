c4d503e32476cc0fd838d0dfbe1bfaee45a0b037
Srinivas Rithin Arakatala <008420653@csusb.edu>
2025-04-20 03:08:31 -0700

    Added Room.java class

--- src/model/Room.java
+++ src/model/Room.java
@@ -1,1 +1,25 @@
+package model;
+
+public class Room {
+    private int roomNumber;
+    private String type;
+    private double price;
+    private boolean isAvailable;
+
+    public Room(int roomNumber, String type, double price, boolean isAvailable) {
+        this.roomNumber = roomNumber;
+        this.type = type;
+        this.price = price;
+        this.isAvailable = isAvailable;
+    }
+
+    public int getRoomNumber() { return roomNumber; }
+    public String getType() { return type; }
+    public double getPrice() { return price; }
+    public boolean isAvailable() { return isAvailable; }
+
+    public void setType(String type) { this.type = type; }
+    public void setPrice(double price) { this.price = price; }
+    public void setAvailable(boolean isAvailable) { this.isAvailable = isAvailable; }
+}
 
